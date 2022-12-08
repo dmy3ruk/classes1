a588b07ec65ef8064d5f9391b6b6dca1de1f4dd3
dmy3ruk <104530429+dmy3ruk@users.noreply.github.com>
2022-12-08 10:52:36 +0200

    Update class2.js

--- class2.js
+++ class2.js
@@ -23,32 +23,3 @@ class Xiaomi{
     }
 }
 console.log(new Ipohe(11,'red',64,'apple'))
-
-
-
-class Phone{
-    constructor(model,color,memory,brand){
-        this.model=model
-        this.color=color
-        this.memory=memory
-        this.brand=brand
-    }
-}
-class Ipohe extends Phone{
-    constructor (model,color,memory,brand){
-        super(model,color,memory)
-        this.brand=brand
-    }
-}
-class Samsung extends Phone{
-    constructor (model,color,memory,brand){
-        super(model,color,memory)
-        this.brand=brand
-    }
-}
-class Xiaomi extends Phone{
-    constructor (model,color,memory,brand){
-        super(model,color,memory)
-        this.brand=brand
-    }
-}console.log(new Samsung("A72","white",256,"samsung"))
